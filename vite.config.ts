5711eb4bd7921d4baedcf7c7b1f21b48b7f2604e
aftandiknamazlo-dotcom <aftandiknamazlo@gmail.com>
2026-04-19 23:44:16 +0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    // Disable service worker in dev mode
-    proxy: {
-      '/sw.js': {
-        target: 'http://localhost:5173',
-        rewrite: () => '',
-        configure: (proxy) => {
-          proxy.on('proxyReq', (_proxyReq, _req, res) => {
-            res.writeHead(200, { 'Content-Type': 'application/javascript' });
-            res.end('// SW disabled in dev mode');
-          });
-        }
-      }
-    }
-  }
+  plugins: [react()]
 })
